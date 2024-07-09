b85d6431b08ce65c461343ccf302e59e555d0e4d
Jaehyeon Kim <dottami@gmail.com>
2024-07-09 17:04:59 +1000

    complete examples

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -1,3 +1,20 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import argparse
 import datetime
 import random
@@ -86,7 +103,7 @@ def run(argv=None, save_main_session=True):
         (
             p
             | "CreateElements" >> beam.Create(create_records(known_args.num_records))
-            | "ConvertTS" >> beam.Map(convert_ts)
+            | "DatetimeToStr" >> beam.Map(convert_ts)
             | "BatchElements" >> BatchElements(min_batch_size=50)
             | "WriteToFirehose"
             >> WriteToFirehose(
